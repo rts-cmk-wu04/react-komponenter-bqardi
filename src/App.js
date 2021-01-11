6e6dc6159deb4eeb2e61c9911023cd5e1fc963dd
bqardi <51706945+bqardi@users.noreply.github.com>
2021-01-11 11:52:54 +0100

    Icon files added + plan

--- src/App.js
+++ src/App.js
@@ -9,3 +9,19 @@ function App() {
 }
 
 export default App;
+
+// SMALL COMPONENTS:
+// Link
+// TopHeading
+// Square
+// Round
+// Icon
+// Button
+
+// SUB COMPONENTS:
+// TitleText (title + text)
+// ImgTxt (Image + TitleText)
+
+// MAJOR COMPONENTS:
+// Layout (this file - App)
+// Card
